1918ec85c625962615675760d372a502ee2ca9ae
kmarius <5224719+kmarius@users.noreply.github.com>
2025-07-15 11:58:51 +0200

    add bulk insert/lookup for timestamps

--- source/kmarius_incremental_scan_db/lib/__init__.py
+++ source/kmarius_incremental_scan_db/lib/__init__.py
@@ -18,21 +18,41 @@ def get_connection():
 def setup(conn):
     cursor = conn.cursor()
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS timestamps (
-           path TEXT PRIMARY KEY,
-           mtime INTEGER NOT NULL
-        )''')
+                   CREATE TABLE IF NOT EXISTS timestamps
+                   (
+                       path
+                       TEXT
+                       PRIMARY
+                       KEY,
+                       mtime
+                       INTEGER
+                       NOT
+                       NULL
+                   )''')
 
 
 def store_timestamp(path, mtime):
     conn = get_connection()
     cur = conn.cursor()
     cur.execute('''
-        INSERT INTO timestamps (path, mtime)
-        VALUES (?, ?) 
-        ON CONFLICT(path) DO UPDATE SET
-            mtime = excluded.mtime
-        ''', (path, mtime))
+                INSERT INTO timestamps (path, mtime)
+                VALUES (?, ?) ON CONFLICT(path) DO
+                UPDATE SET
+                    mtime = excluded.mtime
+                ''', (path, mtime))
+    conn.commit()
+    conn.close()
+
+
+def store_timestamps(values):
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.executemany('''
+                    INSERT INTO timestamps (path, mtime)
+                    VALUES (?, ?) ON CONFLICT(path) DO
+                    UPDATE SET
+                        mtime = excluded.mtime
+                    ''', values)
     conn.commit()
     conn.close()
 
@@ -45,3 +65,16 @@ def load_timestamp(path):
     mtime = row[0] if row else None
     conn.close()
     return mtime
+
+
+def load_timestamps(paths):
+    conn = get_connection()
+    cur = conn.cursor()
+    mtimes = []
+    # there's better approaches for this, e.g. a long in (...) expression with all values, or a CTE
+    for path in paths:
+        cur.execute("SELECT mtime FROM timestamps WHERE path = ?", (path,))
+        row = cur.fetchone()
+        mtimes.append(row[0] if row else None)
+    conn.close()
+    return mtimes
